b3819ec7bd7bffd0d6ab68200860f40d8bc97376
Andrew Nguyen <andrewpn2006@gmail.com>
2026-06-06 15:44:40 -0700

    Added CORS to connect to react frontend

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from . import models
 from .routes import users, auth, blogs, posts
 from .database import engine
+from fastapi.middleware.cors import CORSMiddleware
 
 # Initialize FastAPI app (auto-generates OpenAPI docs at /docs)
 app = FastAPI()
@@ -19,4 +20,21 @@ app.include_router(posts.router)
 @app.get("/")
 def read_root():
     """Root endpoint - health check or API info"""
-    return {"message": "Hello, World!"}
+    return {"message": "Health Check!"}
+
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://localhost:8000",
+    " http://localhost:5173",
+]
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # In production, specify the exact origins
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
